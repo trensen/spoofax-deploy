5d055a45a99a336c5e61c000a6d62ea0a287571a
Gabriël Konat <gabrielkonat@gmail.com>
2013-11-15 12:37:11 +0100

    Update director-make.sh
    Fix incorrect director link.

--- buckminster/director-make.sh
+++ buckminster/director-make.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-wget -O director.zip http://www.eclipse.org/downloads/download.php?file=/tools/buckminster/products/director_latest.zip
+wget -O director.zip http://eclipse.mirror.triple-it.nl/tools/buckminster/products/director_latest.zip
 unzip director.zip
 rm director.zip
