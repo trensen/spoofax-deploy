5e025f9c2227642cd461608a519f04c835267852
Gabriel Konat <gabrielkonat@gmail.com>
2014-05-23 13:27:28 +0200

    Do a clean build instead of an incremental build. Set default JDT compliance level to 1.7 Allow parallel resolution and materialisation. Only show errors

--- buckminster/spoofax-libs/build.sh
+++ buckminster/spoofax-libs/build.sh
@@ -5,8 +5,8 @@ rm -rf out
 mkdir -p out
 
 # Resolve, build and bundle jars
-../headless/buckminster --loglevel INFO -update -data workspace --properties buckminster.properties --script buckminster-resolve.script
-../headless/buckminster --loglevel INFO -update -data workspace --properties buckminster.properties --script buckminster-build.script
+../headless/buckminster --loglevel ERROR -update -data workspace --properties buckminster.properties --script buckminster-resolve.script
+../headless/buckminster --loglevel ERROR -update -data workspace --properties buckminster.properties --script buckminster-build.script
 
 # Merge jars
 sh mkjar.sh
