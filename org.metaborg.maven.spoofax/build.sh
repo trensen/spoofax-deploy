cb94245ead739022ed26bd87cd959720893f05f7
Gabriel Konat <gabrielkonat@gmail.com>
2014-07-07 16:31:48 -0700

    Change plugin projects that do not depend on Eclipse to regular Java projects.

--- org.metaborg.maven.spoofax/build.sh
+++ org.metaborg.maven.spoofax/build.sh
@@ -80,10 +80,12 @@ MAKE_PERMISSIVE_JAR="$GEN_DIST_LOC/make_permissive.jar"
 cp $STRATEGOXT_JAR "$ROOT/strategoxt/strategoxt/stratego-libraries/java-backend/java/strategoxt.jar"
 
 
-# Build and install Java projects
-if [ -z ${NO_JAVA_PROJECTS+x} ]; then
+function maven {
+  MVN_ARGS=$1
+  MVN_FILE=${2:-"pom.xml"}
+
   MAVEN_OPTS="-Xmx1024m -Xms1024m -Xss32m -server -XX:+UseParallelGC" JAVA_HOME=$(/usr/libexec/java_home) mvn \
-    -f "$ROOT/mb-rep/org.spoofax.terms/pom.xml" \
+    -f $MVN_FILE \
     -DforceContextQualifier=$QUALIFIER \
     -Ddist-loc=$GEN_DIST_LOC \
     -Dnative-loc=$NATIVE_LOC \
@@ -91,20 +93,17 @@ if [ -z ${NO_JAVA_PROJECTS+x} ]; then
     -Dsdf2imp-jar=$SDF2IMP_JAR \
     -Dmakepermissive-jar=$MAKE_PERMISSIVE_JAR \
     -Dstrategoxt-jar=$STRATEGOXT_JAR \
-    clean install \
+    $MVN_ARGS \
     $MVN_EXTRA_ARGS
+}
+
+
+# Build and install Java projects
+if [ -z ${NO_JAVA_PROJECTS+x} ]; then
+  maven "clean install" "$ROOT/mb-rep/org.spoofax.terms/pom.xml"
 fi
 
 # Build Spoofax
 if [ -z ${NO_PLUGIN_PROJECTS+x} ]; then
-  MAVEN_OPTS="-Xmx1024m -Xms1024m -Xss32m -server -XX:+UseParallelGC" JAVA_HOME=$(/usr/libexec/java_home) mvn \
-    -DforceContextQualifier=$QUALIFIER \
-    -Ddist-loc=$GEN_DIST_LOC \
-    -Dnative-loc=$NATIVE_LOC \
-    -Daster-jar=$ASTER_JAR \
-    -Dsdf2imp-jar=$SDF2IMP_JAR \
-    -Dmakepermissive-jar=$MAKE_PERMISSIVE_JAR \
-    -Dstrategoxt-jar=$STRATEGOXT_JAR \
-    clean verify \
-    $MVN_EXTRA_ARGS
+  maven "clean verify"
 fi
