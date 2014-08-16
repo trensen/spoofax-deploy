eaf50aeb4e9819350bdeda07c890a499ccd722d1
Gabriel Konat <gabrielkonat@gmail.com>
2014-08-15 17:48:03 -0700

    Wrong variable reference

--- hydra/spoofax.nix
+++ hydra/spoofax.nix
@@ -90,29 +90,29 @@ let
         QUALIFIER=$(./latest-timestamp.sh)
       
       	./spoofax-deploy/org.metaborg.maven.build.strategoxt/build.sh -e ${mavenEnv} -a "-Dmaven.repo.local=$out/.m2"
-      	./spoofax-deploy/org.metaborg.maven.build.java/build.sh -e ${mavenEnv} -a "-Dmaven.repo.local=$out/.m2" -q ${QUALIFIER}
-      	./spoofax-deploy/org.metaborg.maven.build.spoofax.eclipse/build.sh -e ${mavenEnv} -a "-Dmaven.repo.local=$out/.m2" -q ${QUALIFIER}
+      	./spoofax-deploy/org.metaborg.maven.build.java/build.sh -e ${mavenEnv} -a "-Dmaven.repo.local=$out/.m2" -q $QUALIFIER
+      	./spoofax-deploy/org.metaborg.maven.build.spoofax.eclipse/build.sh -e ${mavenEnv} -a "-Dmaven.repo.local=$out/.m2" -q $QUALIFIER
       	./spoofax-deploy/org.metaborg.maven.build.spoofax.libs/build.sh -e ${mavenEnv} -a "-Dmaven.repo.local=$out/.m2"
       	./spoofax-deploy/org.metaborg.maven.build.spoofax.sunshine/build.sh -e ${mavenEnv} -a "-Dmaven.repo.local=$out/.m2"
 
 
         # Set hydra build products.
       	SPOOFAX_SITE_LOC="$out/spoofax-deploy/org.strategoxt.imp.updatesite/target"
-      	SPOOFAX_SITE_FILE="$out/spoofax-eclipse-${QUALIFIER}.tar.gz"
-        touch "''$SPOOFAX_SITE_LOC/index.html"
-        tar -C ''$SPOOFAX_SITE_LOC -cvzf ''$SPOOFAX_SITE_FILE site
+      	SPOOFAX_SITE_FILE="$out/spoofax-eclipse-$QUALIFIER.tar.gz"
+        touch "$SPOOFAX_SITE_LOC/index.html"
+        tar -C $SPOOFAX_SITE_LOC -cvzf $SPOOFAX_SITE_FILE site
         
-        SPOOFAX_LIBS_JAR="$out/spoofax-libs-${QUALIFIER}.jar"
-        cp "$out/spoofax-deploy/org.metaborg.maven.build.spoofax.libs/target/org.metaborg.maven.build.spoofax.libs"*".jar" ''$SPOOFAX_LIBS_JAR
+        SPOOFAX_LIBS_JAR="$out/spoofax-libs-$QUALIFIER.jar"
+        cp "$out/spoofax-deploy/org.metaborg.maven.build.spoofax.libs/target/org.metaborg.maven.build.spoofax.libs"*".jar" $SPOOFAX_LIBS_JAR
         
-        SUNSHINE_JAR="$out/spoofax-sunshine-${QUALIFIER}.jar"
-        cp "$out/spoofax-sunshine/org.spoofax.sunshine/target/org.metaborg.sunshine"*".jar" ''$SUNSHINE_JAR
+        SUNSHINE_JAR="$out/spoofax-sunshine-$QUALIFIER.jar"
+        cp "$out/spoofax-sunshine/org.spoofax.sunshine/target/org.metaborg.sunshine"*".jar" $SUNSHINE_JAR
         
         mkdir -p $out/nix-support
-        echo "file site ''$SPOOFAX_SITE_LOC" >> $out/nix-support/hydra-build-products
-        echo "file tar ''$SPOOFAX_SITE_FILE" >> $out/nix-support/hydra-build-products
-        echo "file jar ''$SPOOFAX_LIBS_JAR" >> $out/nix-support/hydra-build-products
-        echo "file jar ''$SUNSHINE_JAR" >> $out/nix-support/hydra-build-products
+        echo "file site $SPOOFAX_SITE_LOC" >> $out/nix-support/hydra-build-products
+        echo "file tar $SPOOFAX_SITE_FILE" >> $out/nix-support/hydra-build-products
+        echo "file jar $SPOOFAX_LIBS_JAR" >> $out/nix-support/hydra-build-products
+        echo "file jar $SUNSHINE_JAR" >> $out/nix-support/hydra-build-products
       '';
       
       
