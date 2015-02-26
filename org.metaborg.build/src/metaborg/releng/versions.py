5fe5387f97e9cfa12837c4a976c6374a130d460c
Gabriel Konat <gabrielkonat@gmail.com>
2015-02-26 13:53:00 +0100

    Fix common path prefix in set-versions.

--- org.metaborg.build/src/metaborg/releng/versions.py
+++ org.metaborg.build/src/metaborg/releng/versions.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as ET
 
 from os import path
 from metaborg.util.maven import Mvn
+from metaborg.util.path import CommonPrefix
 
 
 def SetVersions(repo, oldMavenVersion, newMavenVersion, dryRun = False, commit = False):
@@ -25,6 +26,7 @@ def SetVersions(repo, oldMavenVersion, newMavenVersion, dryRun = False, commit =
   print('Old version {}'.format(oldVersionString))
   print('New version {}'.format(newVersionString))
 
+
   def FindFiles(root, pattern):
     allFiles = []
     for rootDir, dirs, files in os.walk(root):
@@ -37,19 +39,20 @@ def SetVersions(repo, oldMavenVersion, newMavenVersion, dryRun = False, commit =
     return allFiles
 
   def ReplaceInFile(file, pattern, replacement):
-    print('Setting version in {}'.format(file))
-    changedFiles.append(file)
-    if dryRun:
-      return
     with open(file) as fileHandle:
       text = fileHandle.read()
-    text = text.replace(pattern, replacement)
-    with open(file, "w") as fileHandle:
-      fileHandle.write(text)
+    if pattern in text:
+      print('Setting version in {}'.format(file))
+      text = text.replace(pattern, replacement)
+      changedFiles.append(file)
+      if dryRun:
+        return
+      with open(file, "w") as fileHandle:
+        fileHandle.write(text)
 
   def IsMavenPomFile(file):
     try:
-      xmlRoot = ET.parse(xmlFile)
+      xmlRoot = ET.parse(file)
     except ET.ParseError:
       return False
     project = xmlRoot.getroot()
@@ -57,26 +60,33 @@ def SetVersions(repo, oldMavenVersion, newMavenVersion, dryRun = False, commit =
       return False
     return True
 
+  def IsGeneratedManifestFile(file):
+    with open(file) as fileHandle:
+      text = fileHandle.read()
+
   print('Setting Maven versions in POM files')
-  allXmlFiles = FindFiles(baseDir, '.xml')
-  for xmlFile in allXmlFiles:
-    if IsMavenPomFile(xmlFile):
-      ReplaceInFile(xmlFile, oldMavenVersion, newMavenVersion)
+  for file in FindFiles(baseDir, '.xml'):
+    if IsMavenPomFile(file):
+      ReplaceInFile(file, oldMavenVersion, newMavenVersion)
 
   print('Setting Maven versions in Spoofax generator files')
   createMavenPomFile = path.join(baseDir, 'spoofax', 'org.strategoxt.imp.generator', 'src', 'sdf2imp', 'project', 'create-maven-pom.str')
   ReplaceInFile(createMavenPomFile, oldMavenVersion, oldMavenVersion)
 
+
   print('Setting Eclipse versions in MANIFEST.MF files')
-  allManifestFiles = FindFiles(baseDir, 'MANIFEST.MF')
-  for manifestFile in allManifestFiles:
-    ReplaceInFile(manifestFile, oldEclipseVersion, newEclipseVersion)
+  for file in FindFiles(baseDir, 'MANIFEST.MF'):
+    ReplaceInFile(file, oldEclipseVersion, newEclipseVersion)
 
   print('Setting Eclipse versions in feature.xml files')
   spoofaxDeployDir = path.join(baseDir, 'spoofax-deploy')
-  allFeatureFiles = FindFiles(spoofaxDeployDir, 'feature.xml')
-  for featureFile in allFeatureFiles:
-    ReplaceInFile(featureFile, oldEclipseVersion, newEclipseVersion)
+  for file in FindFiles(spoofaxDeployDir, 'feature.xml'):
+    ReplaceInFile(file, oldEclipseVersion, newEclipseVersion)
+
+  print('Setting Eclipse versions in site.xml files')
+  for file in FindFiles(spoofaxDeployDir, 'site.xml'):
+    ReplaceInFile(file, oldEclipseVersion, newEclipseVersion)
+
 
   if commit:
     print('Committing changed files')
@@ -84,8 +94,11 @@ def SetVersions(repo, oldMavenVersion, newMavenVersion, dryRun = False, commit =
       print('Submodule {}'.format(submodule.name))
       subrepo = submodule.module()
       subrepoPath = subrepo.working_dir
-      filesToAdd = [path.relpath(f, subrepoPath) for f in changedFiles if path.commonprefix([subrepoPath, f]) == subrepoPath]
-      print('Adding files {} and committing'.format(filesToAdd))
-      if not dryRun:
-        subrepo.index.add(filesToAdd)
-        subrepo.index.commit('Set version to {}'.format(newVersionString))
+      filesToAdd = [path.relpath(f, subrepoPath) for f in changedFiles if CommonPrefix([subrepoPath, f]) == subrepoPath]
+      if len(filesToAdd) != 0:
+        if dryRun:
+          print('Changed files {}'.format(filesToAdd))
+        else:
+          print('Adding files {} and committing'.format(filesToAdd))
+          if len(subrepo.index.add(filesToAdd)) != 0:
+            subrepo.index.commit('Set version to {}'.format(newVersionString))
