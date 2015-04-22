0778e3537b36b699a9bf40710160b48990e74cea
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2015-04-22 20:33:05 +0200

    Always install Python packages as user.

--- org.metaborg.build/run.sh
+++ org.metaborg.build/run.sh
@@ -5,7 +5,7 @@ set -eu
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if command -v pip3 >/dev/null 2>&1; then
-  pip3 install -q -r "$DIR/requirements.txt"
+  pip3 install --user -q -r "$DIR/requirements.txt"
 else
   echo "Cannot find 'pip3' to install required libraries, it should be included with a recent python 3 installation"
 fi
