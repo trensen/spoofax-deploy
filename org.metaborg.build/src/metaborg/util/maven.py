c858644d09309978e37027bd50ec81a087b6772a
Gabriel Konat <gabrielkonat@gmail.com>
2015-07-30 10:48:20 -0700

    Add Maven options switch

--- org.metaborg.build/src/metaborg/util/maven.py
+++ org.metaborg.build/src/metaborg/util/maven.py
@@ -3,11 +3,12 @@ import platform
 import subprocess
 import pystache
 
+_mavenOpts = '-Xms512m -Xmx1024m -Xss32m -XX:MaxPermSize=512m'
 
 def Mvn(pomFile = 'pom.xml', settingsFile = None, globalSettingsFile = None, localRepo = None,
  noSnapshotUpdates = False, forceSnapshotUpdate = False, offline = False, skipTests = False,
  profiles = [], debug = False, quiet = False, extraArgs = None, clean = True, phase = 'verify',
- resumeFrom = None, mavenOpts = '-Xms512m -Xmx1024m -Xss32m -XX:MaxPermSize=512m', **kwargs):
+ resumeFrom = None, mavenOpts = _mavenOpts, **kwargs):
   args = []
   if platform.system() == 'Windows':
     args.append('mvn.bat')
